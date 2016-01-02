bb4ce0695c22c0943fc379d9036e74851dc00691
jiikko <n905i.1214@gmail.com>
2016-01-02 10:44:45 +0900

    はいい

--- mysql/multi_index/init.rb
+++ mysql/multi_index/init.rb
@@ -2,7 +2,7 @@ require "mysql2"
 require "active_record"
 require 'sugoi_bulk_insert'
 
-DB_NAME = "has_many_joins"
+DB_NAME = "multi_index"
 
 puts `echo 'drop database #{DB_NAME}' | mysql -uroot`
 puts `echo 'create database #{DB_NAME}' | mysql -uroot`
@@ -20,9 +20,10 @@ config = {
 ActiveRecord::Base.establish_connection(config)
 ActiveRecord::Migration.create_table :employees do |t|
   t.string :name
+  t.integer :account_id
   t.integer :department_id
 end
-ActiveRecord::Migration.add_index :employees, :department_id
+ActiveRecord::Migration.add_index :employees, [:account_id, :department_id]
 ActiveRecord::Migration.create_table :departments do |t|
   t.string :name
 end
@@ -30,22 +31,18 @@ end
 class Employee < ActiveRecord::Base
   belongs_to :department
 end
-class Department  < ActiveRecord::Base;
+class Department < ActiveRecord::Base
   has_many :employees
 end
 class Comment < ActiveRecord::Base
 end
 
-
-# 部署1と社員1
-
-50_000.times do |i|
-  Department.create!(name: "部署#{i}").employees.create!(name: "人#{i}")
+b = SugoiBulkInsert.new(table_name: "employees", count: 100000) do |x|
+  x.column :name, 'aaaa'
+  x.column :account_id, 1...40000
+  x.column :department_id, 1...40000
 end
-# select count(*) FROM departments D where exists (select E.department_id from employees E where E.department_id = D.id);
-# SELECT distinct count(*) FROM `departments` INNER JOIN `employees` ON `employees`.`department_id` = `departments`.`id`;
-# countはjoinのほうが早い
-#
-#
-# これ
-#
+6.times { b.fire }
+
+Employee.where(account_id: 3)     # インデックス聞く
+Employee.where(department_id: 3)  # 効かない
