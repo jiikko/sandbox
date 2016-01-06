753769025dac6c91ec7a335266051a0826cb0067
jiikko <n905i.1214@gmail.com>
2016-01-07 00:07:49 +0900

    はい

---
+++ mysql/README.md
@@ -0,0 +1,9 @@
+# sandbox/mysql
+## 使い方
+
+```
+$ pwd
+/path/to/sandbox/mysql
+$ bundle exec irb
+$ require "./covering_index/init"
+```
--- mysql/covering_index/init.rb
+++ mysql/covering_index/init.rb
@@ -1,6 +1,10 @@
+# cd ./mysql/covering_index
+# $ bunsle exec irb
+# require "covering_index/init"
+
 DB_NAME = "covering_index"
 
-require '../mysql_helper'
+require './mysql_helper'
 
 ActiveRecord::Migration.create_table :employees do |t|
   t.string :name
@@ -17,3 +21,29 @@ end
 class Department  < ActiveRecord::Base;
   has_many :employees
 end
+
+
+b = SugoiBulkInsert.new(table_name: "employees", count: 100000) do |x|
+  x.column :name, 'aaaa'
+  x.column :department_id, 1...40000
+end
+2.times { b.fire }
+
+
+# 2.3.0 :013 > Employee.where(department_id: 2).explain
+#  => EXPLAIN for: SELECT `employees`.* FROM `employees` WHERE `employees`.`department_id` = 2
+# +----+-------------+-----------+------------+------+----------------------------------+----------------------------------+---------+-------+------+----------+-------+
+# | id | select_type | table     | partitions | type | possible_keys                    | key                              | key_len | ref   | rows | filtered | Extra |
+# +----+-------------+-----------+------------+------+----------------------------------+----------------------------------+---------+-------+------+----------+-------+
+# |  1 | SIMPLE      | employees | NULL       | ref  | index_employees_on_department_id | index_employees_on_department_id | 5       | const |    9 |    100.0 | NULL  |
+# +----+-------------+-----------+------------+------+----------------------------------+----------------------------------+---------+-------+------+----------+-------+
+# 1 row in set (0.00 sec)
+#
+# 2.3.0 :014 > Employee.where(department_id: 2).select(:department_id).explain
+#  => EXPLAIN for: SELECT `employees`.`department_id` FROM `employees` WHERE `employees`.`department_id` = 2
+# +----+-------------+-----------+------------+------+----------------------------------+----------------------------------+---------+-------+------+----------+-------------+
+# | id | select_type | table     | partitions | type | possible_keys                    | key                              | key_len | ref   | rows | filtered | Extra       |
+# +----+-------------+-----------+------------+------+----------------------------------+----------------------------------+---------+-------+------+----------+-------------+
+# |  1 | SIMPLE      | employees | NULL       | ref  | index_employees_on_department_id | index_employees_on_department_id | 5       | const |    9 |    100.0 | Using index |
+# +----+-------------+-----------+------------+------+----------------------------------+----------------------------------+---------+-------+------+----------+-------------+
+# 1 row in set (0.00 sec)
