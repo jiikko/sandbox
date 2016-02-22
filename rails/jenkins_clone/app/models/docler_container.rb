fe6369566031da1e4ce9142fc8d0c836f7c72223
jiikko <n905i.1214@gmail.com>
2016-02-22 22:06:00 +0900

    う

--- rails/jenkins_clone/app/models/docler_container.rb
+++ rails/jenkins_clone/app/models/docler_container.rb
@@ -1,3 +1,5 @@
+require "open3"
+
 class DoclerContainer < ActiveRecord::Base
   def test
     cmd = <<-CMD
@@ -9,6 +11,14 @@ ls
   end
 
   def hai
+
+    Open3.popen3("echo a; sleep 1; echo b; sort >&2; sleep 3") do |i, o, e, w|
+      i.write "foo\nbar\nbaz\n"
+      i.close
+      o.each do |line| p line end #=> "a\n",  "b\n"
+      e.each do |line| p line end #=> "bar\n", "baz\n", "foo\n"
+      p w.value #=> #<Process::Status: pid 32682 exit 0>
+    end
     `ls`
     `ls`
     `ls`
