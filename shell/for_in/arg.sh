31effadec679d210d9d3e40cf7243986d3ff4ad1
jiikko <n905i.1214@gmail.com>
2016-05-04 14:48:59 +0900

    fix

--- shell/for_in/arg.sh
+++ shell/for_in/arg.sh
@@ -3,23 +3,23 @@
 
 # loopされる
 ls="README.md docs hoge.sh mysql rails ruby shell vimscript"
-for i in $ls; do
-  echo $i
-done
+for i in $ls; do echo $i; done
 
 # loopされない
-echo '----------'
-for i in "README.md docs hoge.sh mysql rails ruby shell vimscript"; do
-  echo $i
-done
+for i in "README.md docs hoge.sh mysql rails ruby shell vimscript"; do echo $i; done
+
+# loopされない
+for i in "echo README.md docs hoge.sh mysql rails ruby shell vimscript"; do echo $i; done
 
 # loopされる
-echo '----------'
-for i in `ls`; do
-  echo $i
-done
-
 list="echo README.md docs hoge.sh mysql rails ruby shell vimscript"
-for i in $list; do
-  echo $i
-done
+for i in $list; do echo $i; done
+
+# loopされる
+for i in `echo README.md docs hoge.sh mysql rails ruby shell vimscript`; do echo $i; done
+
+# loopされる
+for i in $(echo README.md docs hoge.sh mysql rails ruby shell vimscript); do echo $i; done
+
+# loopされる
+for i in `ls`; do echo $i; done
