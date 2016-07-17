e7f2feb0857ae338c4023456a20d931046c827fb
jiikko <n905i.1214@gmail.com>
2016-07-17 23:11:57 +0900

    shell/background_job/nohup.sh

--- shell/background_job/nohup.sh
+++ shell/background_job/nohup.sh
@@ -1,4 +1,12 @@
-# ssh 接続先のサーバーで nohup ジョブを走らせるときは 標準入出力をすべて切っておかないと期待通りにsshを抜けられない:(sleep 10; touch /tmp/test_qqq) &
+# ssh 接続先のサーバーで nohup ジョブを走らせるときは 標準入出力をすべて切っておかないと期待通りにsshを抜けられない
 
-echo 'create database outing_development;' | mysql -uroot
-nohup $(mysqldump -uroot outing_production | mysql -uroot outing_development) >/dev/null 2>/dev/null &
+nohup bash -c 'sleep 10; touch one; touch 3; touch two' >/dev/null 2>/dev/null &
+
+cmd=$(cat << EOH
+sleep 1
+touch aaa
+sleep 1
+touch bbb
+EOH
+)
+nohup bash <<< "$cmd" >/dev/null 2>/dev/null &
