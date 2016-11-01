d0f79222f22daaadfceba62439c1cd88b0682cc7
jiikko <n905i.1214@gmail.com>
2016-11-01 15:19:19 +0900

    fix make release branch command

--- bin/make_release_branch.sh
+++ bin/make_release_branch.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-(git branch -D d/11111-upgrade4.2-release)
+git checkout master
+git branch -D d/11111-upgrade4.2-release
 git checkout master
 git checkout -b d/11111-upgrade4.2-release
 
-/bin/sh <<< $(ruby -e '%w(d/17796-upgrade-rails4.2              \
-                          d/17796-upgrade-rails4.2-deliver_now2 \
-                          d/17796-upgrade-rails4.2-where-not    \
-                       ).each { |b| puts "git merge #{b}" }')
+if [[ $(git branch | grep \* | grep master) ]]; then
+  echo 'stop!!!!! because current is mastter'
+else
+  git merge d/17796-upgrade-rails4.2
+  git merge d/17796-upgrade-rails4.2-deliver_now2
+  git merge d/17796-upgrade-rails4.2-where-not
+fi
