a050a13a7c9ff3d8d02f2d8ecb132ae005e482b0
jiikko <n905i.1214@gmail.com>
2016-11-06 15:06:37 +0900

    add tree lib

--- ruby/tree/tree_sorted_list/tree_sorted_list.rb
+++ ruby/tree/tree_sorted_list/tree_sorted_list.rb
@@ -1,6 +1,10 @@
 require 'pry'
 
 class TreeSortedList
+  class << self
+    attr_accessor :divisors
+  end
+
   class Node
     def initialize(root)
       @root = root
@@ -8,7 +12,6 @@ class TreeSortedList
 
     def calc
       r = []
-      binding.pry
       if next_diff.zero?
         return @root
       end
@@ -35,11 +38,18 @@ class TreeSortedList
 
   def initialize(root)
     @node = Node.new(root)
+    TreeSortedList.divisors =
   end
 
   def generate
     @node.calc
   end
+
+  def create_divisors(i)
+    # http://qiita.com/seinosuke/items/fde2e0471dcf937e5a09
+    # http://syoshinsyakangeisagi.blogspot.jp/2014/12/ruby_5.html
+  end
 end
 
 list = TreeSortedList.new(5).generate
+
