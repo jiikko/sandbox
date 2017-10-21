00890d9028fb19770b09c0e5a723bf0cd47f7455
jiikko <n905i.1214@gmail.com>
2017-10-21 16:02:38 +0900

    es6への書き換え途中

--- js/template/react-biginners/js/source/components/textarea_counter.js
+++ js/template/react-biginners/js/source/components/textarea_counter.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 class TextareaCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'default value' };
+    // https://qiita.com/cubdesign/items/ee8bff7073ebe1979936#%E8%A7%A3%E6%B1%BA%E7%AD%96%EF%BC%91
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
   handleOnChange(env) {
-    console.log(env)
+    console.log(env.target.value);
+    this.setState = { text: env.target.value };
   }
   render() {
     return <textarea
-            value="ggggggg"
             onChange={this.handleOnChange}
            ></textarea>;
   }
