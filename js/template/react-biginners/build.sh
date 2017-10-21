0b5e6ad3a19ae01806af4d6255dbcfbf6b3aa0ee
jiikko <n905i.1214@gmail.com>
2017-10-22 01:30:35 +0900

    Upgrade build.sh for es6 compile tool

--- js/template/react-biginners/build.sh
+++ js/template/react-biginners/build.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-CMD="babel --presets react,es2015 js/source -d js/build && browserify ./js/build/app.js -o bundle.js"
+CMD=$(cat <<-EOF
+babel --presets react,es2015 js/source -d js/build && browserify ./js/build/app.js -o bundle.js;
+if [[ \$? = 0 ]]; then
+  RESULT="success compile"
+else
+  RESULT="failure compile"
+fi
+echo \$RESULT | terminal-notifier
+EOF
+)
 $(npm bin)/watch "$CMD" js/source
