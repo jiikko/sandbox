24b0f98b9cdde834756cf0f0ade24e637d2fd878
jiikko <n905i.1214@gmail.com>
2018-01-23 18:23:05 +0900

    ほげぇ、を表示する

--- obj-c/cmdtools/sample/main.m
+++ obj-c/cmdtools/sample/main.m
@@ -1,7 +1,8 @@
 #import "anyClass.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
     NSString *hoge = @"test";
-    printf("String is %s", [hoge UTF8String]);
+    printf("String is %s\n", [hoge UTF8String]);
+    printf("String is %s\n", [@"ほげぇ" UTF8String]);
     return 0;
 }
