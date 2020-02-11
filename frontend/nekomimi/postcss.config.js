4a4d63e8fab4194a1ab79eeccf1e65f85dfcdea7
jiikko <n905i.1214@gmail.com>
2020-02-11 14:26:31 +0900

    Add postcss-nesting

---
+++ frontend/nekomimi/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      overrideBrowserslist: ['> 0.25%', 'not op_mini all']
+    },
+    'postcss-custom-properties': {},
+    'postcss-nesting': {}
+  }
+};
