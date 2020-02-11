3651350a2f6574502cb132caf618fccd330902ce
jiikko <n905i.1214@gmail.com>
2020-02-11 18:06:18 +0900

    Split webpack.config.js with production and development

--- frontend/nekomimi/webpack.common.js
+++ frontend/nekomimi/webpack.common.js
@@ -1,30 +1,17 @@
 const path = require('path');
-
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSEXtractPlugin = require('mini-css-extract-plugin');
 
 const src = path.join(__dirname, 'src');
-const dist = path.join(__dirname, 'dist');
 
 module.exports = {
-  mode: 'development',
   entry: path.resolve(src, 'js/render.jsx'),
-  output: {
-    filename: 'index.bundle.js',
-    path: dist
-  },
   resolve: {
     modules: ['node_modules'],
     extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        enforce: 'pre',
-        loader: 'eslint-loader'
-      },
       { test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
@@ -46,13 +33,7 @@ module.exports = {
       }
     ]
   },
-  devServer: {
-    contentBase: dist,
-    hot: true,
-    port: 3000
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(src, 'html/index.html')
     }),
@@ -60,4 +41,4 @@ module.exports = {
       filename: 'app.css',
     }),
   ]
-};
+}
