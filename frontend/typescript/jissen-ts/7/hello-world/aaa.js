01b523ab114369e703aef1b48f70b92337f7c356
jiikko <n905i.1214@gmail.com>
2020-03-20 17:20:59 +0900

    rm .js

--- frontend/typescript/jissen-ts/7/hello-world/aaa.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var React = require("react");
-var Child = function (props) { return (<div>{props.children}</div>); };
-var Parent = function (props) { return (<Child>
-  {props.childLabel}
-  </Child>); };
