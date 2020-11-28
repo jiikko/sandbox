ad9f2d86e33b0a6a3fb85fcbe25fefc97ba265ba
jiikko <n905i.1214@gmail.com>
2020-11-28 20:36:13 +0900

    map storyboard and viewcontroller

--- ios/mapMap/mapMap/ViewController.swift
+++ ios/mapMap/mapMap/ViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var dispMap: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
