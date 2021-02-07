35d3305b9f0d8c7c68bf8d099804b4745e430763
jiikko <n905i.1214@gmail.com>
2021-02-07 18:54:18 +0900

    hi

--- ruby/small-rails/server.rb
+++ ruby/small-rails/server.rb
@@ -1,11 +1,18 @@
 require 'socket'
+require 'pry'
+
+class Application
+  def call(env)
+    [200, {}, ["hellow #{Time.now.to_i}"]]
+  end
+end
 
 port = 3000
 server = TCPServer.open port
-
-socket = server.accept
+puts "start!"
 
 loop do
+  socket = server.accept
   puts "got connection!"
 
   if match = socket.gets.chomp.match(/^(?<verb>[A-Z]*) (?<path>[^ ]*) (?<ver>.*)$/)
@@ -14,17 +21,28 @@ loop do
       break if line.bytesize == 0
       headers << line.split(": ")
     end
-    p VERB: match[:verb]
-    p PATH: match[:path]
-    p VERSION: match[:ver]
-    p HEADERS: headers
+    env = {
+      REQUEST_METHOD: match[:verb],
+      PATH_INFO: match[:path],
+      HTTP_VERSION: match[:ver],
+    }
 
-    if line.bytesize == 0
-      socket.write "HTTP/1.1 200 OK"
-      socket.write "\r\n"
-      socket.write "Hellow \r\n"
-      socket.close
-      break
+    app = Application.new
+    response = app.call(env)
+
+    status = response[0]
+    headers = response[1]
+    body = response[2]
+
+    socket.write "HTTP/1.1 #{status} OK\r\n"
+    headers.each do |key, value|
+      socket.write "#{key}: #{value}"
     end
+    socket.write "\r\n"
+    body.each { |part| socket.write part }
+    socket.close
+    puts 'next!'
+  else
+    socket.close
   end
 end
