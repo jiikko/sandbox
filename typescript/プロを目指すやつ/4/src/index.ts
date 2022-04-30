01670b43dcd74196ef9120c4b1708a1ca135e9bc
jiikko <n905i.1214@gmail.com>
2022-04-30 13:18:47 +0900

    nu

--- typescript/プロを目指すやつ/4/src/index.ts
+++ typescript/プロを目指すやつ/4/src/index.ts
@@ -1,3 +1,4 @@
+/*
 function repeat<T>(element: T, length: number): T[] {
   const result: T[] = [];
   for(let i = 0; i < length; i++) {
@@ -14,10 +15,25 @@ function repeat2<T>(element: T): T[] {
   return [element];
 }
 
-console.log(repeat2<string>("a", 5));
-console.log(repeat<number>(123, 2));
+console.log(repeat2<string>("a"));
+console.log(repeat2<number>(123));
 
 
-function repeat2<T>(element: T): T[] {
+function repeat3<T>(element: T): T[] {
   return [element];
 }
+
+
+
+function repeat4<T>(element: T): T[] {
+  return [element];
+}
+console.log(repeat4(123));
+
+
+
+function repeat5<T, Z>(element: T, zoo: Z): Array<T | Z> {
+  return [element, zoo];
+}
+console.log(repeat5(123, "foo"));
+*/
